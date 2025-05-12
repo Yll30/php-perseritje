8b32d59c8bedb2bfebb84ec7e512d975be7d8656
Yll30 <ylli.gashi208@gmail.com>
2025-05-12 17:44:21 +0200

    .

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+include_once("config.php");
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+}
+?>
